1fcde586ebf67dbc771594d0f464db1a5165170d
adenh93 <aden.herold1@gmail.com>
2023-04-15 15:24:55 +1000

    feat: added boilerplate for basic api with health_check route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use entertainment_api::routes::health_check;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(health_check));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
